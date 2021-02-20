b9de2c2a33f90f6700720b6661bb36cdb165e5c6
IkumaTadokoro <tadokorodev@gmail.com>
2021-02-20 10:09:05 +0900

    Gameクラスのテスト名を修正

--- 07.bowling_object/test/game_test.rb
+++ 07.bowling_object/test/game_test.rb
@@ -7,7 +7,7 @@ require_relative '../lib/roll'
 
 class GameTest < Minitest::Test
   def test_score_no_spare_and_no_strike
-    game = Game.new('2,2,5,3,4,3,1,6,8,1,3,6,3,3,7,2,1,8,1,5,')
+    game = Game.new('2,2,5,3,4,3,1,6,8,1,3,6,3,3,7,2,1,8,1,5')
     assert_equal 74, game.score
   end
 
@@ -26,27 +26,27 @@ class GameTest < Minitest::Test
     assert_equal 0, game.score
   end
 
-  def test_score_prepared_by_practice_a
+  def test_score_spare_at_last_frame_prepared_by_practice
     game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
     assert_equal 139, game.score
   end
 
-  def test_score_prepared_by_practice_b
+  def test_score_strike_at_last_frame_prepared_by_practice
     game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
     assert_equal 164, game.score
   end
 
-  def test_score_prepared_by_practice_c
+  def test_score_no_spare_and_no_strike_at_last_frame_prepared_by_practice
     game = Game.new('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
     assert_equal 107, game.score
   end
 
-  def test_score_prepared_by_practice_d
+  def test_score_strike_at_last_frame_again_prepared_by_practice
     game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
     assert_equal 134, game.score
   end
 
-  def test_score_prepared_by_practice_e
+  def test_score_perfect_prepared_by_practice
     game = Game.new('X,X,X,X,X,X,X,X,X,X,X,X')
     assert_equal 300, game.score
   end
